f7eba92bf2068957a3d7e345e9646a9a3daed476
KD24-ENG <62015005@kmitl.ac.th>
2022-04-25 17:14:06 +0700

    second commit

---
+++ service/connect.php
@@ -0,0 +1,8 @@
+<?php
+    $servername = "localhost";
+    $username = "root";
+    $password = "050819";
+    $dbname = "bkkdb";
+
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+?>
